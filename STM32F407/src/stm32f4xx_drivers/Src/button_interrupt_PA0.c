437b238e9b5f60d6d6f28cab4a26d23b110df38a
shak48 <61759664+shak48@users.noreply.github.com>
2021-12-17 14:55:09 -0500

    Update button_interrupt_PA0.c

--- STM32F407/src/stm32f4xx_drivers/Src/button_interrupt_PA0.c
+++ STM32F407/src/stm32f4xx_drivers/Src/button_interrupt_PA0.c
@@ -1,4 +1,6 @@
 #include <stdint.h>
+#include <string.h>
+
 #include "stm32f407xx.h"
 #include "stm32f407xx_gpio_driver.h"
 
@@ -9,12 +11,14 @@
 int main(void){
 
 	GPIO_Handle_t GpioLedGreen, GpioBtn;
+	memset(&GpioLedGreen,0,sizeof(GpioLedGreen));
+	memset(&GpioBtn,0,sizeof(GpioBtn));
 
 	GpioBtn.pGPIOx = GPIOA;
 	GpioBtn.GPIO_PinConfig.GPIO_PinNumber = GPOIO_PIN_NO_0;
 	GpioBtn.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_IT_FT;
 	GpioBtn.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
-	GpioBtn.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_PIN_PU;
+	//GpioBtn.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_PIN_PD;
 
 	GpioLedGreen.pGPIOx = GPIOD;
 	GpioLedGreen.GPIO_PinConfig.GPIO_PinNumber = GPOIO_PIN_NO_12;
@@ -33,14 +37,13 @@ int main(void){
 	//GPIO_IRQPriorityConfig(IRQ_NO_EXTI0, NVIC_IRQ_PRIO15);
 	GPIO_IRQInterruptConfig(IRQ_NO_EXTI0, ENABLE);
 
-
-
-
-	while(1);
+	for(;;);
 }
 
 void EXTI0_IRQHandler(void)
 {
+	for(int32_t i=0;i<=5000;i++){;}
+
 	GPIO_IRQHandling(GPOIO_PIN_NO_0);
 	GPIO_ToggleOutputPin(GPIOD, GPOIO_PIN_NO_12);
 }
