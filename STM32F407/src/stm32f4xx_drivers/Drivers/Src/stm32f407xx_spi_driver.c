db398415462bbd072cebbc273ea447337b38f633
shak48 <61759664+shak48@users.noreply.github.com>
2021-12-20 17:48:54 -0500

    Update stm32f407xx_spi_driver.c
    Completed SPI

--- STM32F407/src/stm32f4xx_drivers/Drivers/Src/stm32f407xx_spi_driver.c
+++ STM32F407/src/stm32f4xx_drivers/Drivers/Src/stm32f407xx_spi_driver.c
@@ -68,6 +68,7 @@ void SPI_PCLK_Control(SPI_RegDef_t *pSPIx, uint8_t En_Or_Di)
 }
 void SPI_Init(SPI_Handle_t *pSPIHandle)
 {
+	SPI_PCLK_Control(pSPIHandle->pSPIx, ENABLE);
 	uint32_t temp =0;
 	//device mode
 	temp |= pSPIHandle->SPI_Config.SPI_DviceMode << SPI_CR1_MSTR;
@@ -92,6 +93,9 @@ void SPI_Init(SPI_Handle_t *pSPIHandle)
 
 	temp |= pSPIHandle->SPI_Config.SPI_CPHA <<SPI_CR1_CPHA;
 
+	temp |= pSPIHandle->SPI_Config.SPI_SSM <<SPI_CR1_SSM;
+
+
 	pSPIHandle->pSPIx->CR1 |= temp;
 
 
@@ -100,14 +104,62 @@ void SPI_Init(SPI_Handle_t *pSPIHandle)
 void 		SPI_DeInit(SPI_RegDef_t *pSPIx);
 
 
-//data RX TX
-
-void SPI_SendData(SPI_RegDef_t *pSPIx, uint8_t *pTxBuffer, uint32_t Len)
+uint8_t  SPI_GetFlagStatus(SPI_RegDef_t *pSPIx, uint32_t flagName)
 {
+	if(pSPIx->SR & flagName)
+	{
+		return FLAG_SET;
+	}
 
+	return FLAG_RESET;
 }
 
 
+//data RX TX
+
+//this is blocking blocking// polling
+void SPI_SendData(SPI_RegDef_t *pSPIx, uint8_t *pTxBuffer, uint32_t Len)
+{
+	while(Len >0)
+	{
+		while(SPI_GetFlagStatus(pSPIx, SPI_TXE_FLAG) == FLAG_RESET);
+
+		if((pSPIx->CR1 & (1<<SPI_CR1_DFF)))
+		{
+			pSPIx->DR = *((uint16_t*)(pTxBuffer));
+			Len--;
+			Len--;
+			(uint16_t*)pTxBuffer++;
+		}else
+		{
+			pSPIx->DR = *pTxBuffer;
+			Len--;
+			pTxBuffer++;
+		}
+	}
+}
+
+void SPI_Peri_Control(SPI_RegDef_t *pSPIx, uint8_t En_Or_Di)
+{
+	if(En_Or_Di == ENABLE)
+	{
+		pSPIx->CR1 |= (1 << SPI_CR1_SPE);
+	}else
+	{
+		pSPIx->CR1 &= ~(1 << SPI_CR1_SPE);
+	}
+}
+
+void SPI_SSM_Control(SPI_RegDef_t *pSPIx, uint8_t En_Or_Di)
+{
+	if(En_Or_Di == ENABLE)
+	{
+		pSPIx->CR1 |= (1 << SPI_CR1_SSM);
+	}else
+	{
+		pSPIx->CR1 &= ~(1 << SPI_CR1_SSM);
+	}
+}
 
 void SPI_ReceiveData(SPI_RegDef_t *pSPIx,uint8_t *pRxBuffer, uint32_t Len);
 
