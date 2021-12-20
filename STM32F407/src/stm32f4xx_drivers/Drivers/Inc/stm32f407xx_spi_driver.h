6eef90b8a834483701b9ecd26cb263f202942c78
shak48 <61759664+shak48@users.noreply.github.com>
2021-12-20 17:47:50 -0500

    Update stm32f407xx_spi_driver.h

--- STM32F407/src/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_spi_driver.h
+++ STM32F407/src/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_spi_driver.h
@@ -5,7 +5,7 @@
  *      Author: shak48
  */
 
-#include "stm32f407xx.h"
+#include <stm32f407xx.h>
 
 #ifndef INC_STM32F407XX_SPI_DRIVER_H_
 #define INC_STM32F407XX_SPI_DRIVER_H_
@@ -28,6 +28,57 @@ typedef struct
 }SPI_Handle_t;
 
 
+//bit pos definituon for spi cr1
+
+#define SPI_CR1_CPHA		0
+#define SPI_CR1_CPOL		1
+#define SPI_CR1_MSTR		2
+#define SPI_CR1_BR			3
+#define SPI_CR1_SPE			6
+#define SPI_CR1_LSB			7
+#define SPI_CR1_SSI			8
+#define SPI_CR1_SSM			9
+#define SPI_CR1_RXONLY		10
+#define SPI_CR1_DFF			11
+#define SPI_CR1_CRCNEXT		12
+#define SPI_CR1_CRCEN		13
+#define SPI_CR1_BIDIOE		14
+#define SPI_CR1_BIDIMODE	15
+
+
+//bit definition mcro for cr2
+
+#define SPI_CR2_RXMAEN		0
+#define SPI_CR2_TXMAEN		1
+#define SPI_CR2_SSOE		2
+#define SPI_CR2_FRF			4
+#define SPI_CR2_ERRIE		5
+#define SPI_CR2_RXEIE		6
+#define SPI_CR2_TXEIE		7
+
+//spi sr
+#define SPI_SR_RXNE			0
+#define SPI_SR_TXE			1
+#define SPI_SR_CHSIDE		2
+#define SPI_SR_UDR			3
+#define SPI_SR_CRCERR		4
+#define SPI_SR_MODEF		5
+#define SPI_SR_OVR			6
+#define SPI_SR_BSY			7
+#define SPI_SR_FRE			8
+
+
+#define SPI_RXE_FLAG		(1 << SPI_SR_RXNE)
+#define SPI_TXE_FLAG		(1 << SPI_SR_TXE)
+#define SPI_CHSIDE_FLAG		(1 << SPI_SR_CHSIDE)
+#define SPI_UDR_FLAG		(1 << SPI_SR_UDR)
+#define SPI_CRCERR_FLAG		(1 << SPI_SR_CRCERR)
+#define SPI_MODEF_FLAG		(1 << SPI_SR_MODEF)
+#define SPI_OVR_FLAG		(1 << SPI_SR_OVR)
+#define SPI_BSY_FLAG		(1 << SPI_SR_BSY)
+#define SPI_FRE_FLAG		(1 << SPI_SR_FRE)
+
+
 // API prototypes
 
 
@@ -47,6 +98,13 @@ void SPI_ReceiveData(SPI_RegDef_t *pSPIx,uint8_t *pRxBuffer, uint32_t Len);
 void 		SPI_IRQInterruptConfig(uint8_t IRQNumber, uint8_t En_Or_Di);
 void 		SPI_IRQPriorityConfig(uint8_t IRQNumber, uint16_t IRQPriority);
 void 		SPI_IRQHandling(SPI_Handle_t *pSPIHandle);
+
+
+//other perri control
+
+void SPI_Peri_Control(SPI_RegDef_t *pSPIx, uint8_t En_Or_Di);
+
+void SPI_SSM_Control(SPI_RegDef_t *pSPIx, uint8_t En_Or_Di);
 
 
 #endif /* INC_STM32F407XX_SPI_DRIVER_H_ */
