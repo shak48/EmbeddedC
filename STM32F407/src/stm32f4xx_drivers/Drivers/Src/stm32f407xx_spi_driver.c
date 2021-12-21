ce9c583b4d018b632fed6642328c2083480e8a6d
shak48 <61759664+shak48@users.noreply.github.com>
2021-12-21 16:30:28 -0500

    Update stm32f407xx_spi_driver.c

--- STM32F407/src/stm32f4xx_drivers/Drivers/Src/stm32f407xx_spi_driver.c
+++ STM32F407/src/stm32f4xx_drivers/Drivers/Src/stm32f407xx_spi_driver.c
@@ -1,96 +1,166 @@
 /*
- * stm32f407xx_spi_driver.h
+ * stm32f407xx_spi_driver.c
  *
- *  Created on: Dec 19, 2021
+ *  Created on: Dec 18, 2021
  *      Author: shak48
  */
 
-#include <stm32f407xx.h>
-
-#ifndef INC_STM32F407XX_SPI_DRIVER_H_
-#define INC_STM32F407XX_SPI_DRIVER_H_
-
-typedef struct
-{
-	uint8_t SPI_DviceMode;
-	uint8_t SPI_BusConfig;
-	uint8_t SPI_SclkSpeed;
-	uint8_t SPI_DFF;
-	uint8_t SPI_CPHA;
-	uint8_t SPI_CPOL;
-	uint8_t SPI_SSM;
-}SPI_Config_t;
-
-typedef struct
-{
-	SPI_RegDef_t *pSPIx;
-	SPI_Config_t SPI_Config;
-}SPI_Handle_t;
-
-
-//bit pos definituon for spi cr1
-
-#define SPI_CR1_CPHA		0
-#define SPI_CR1_CPOL		1
-#define SPI_CR1_MSTR		2
-#define SPI_CR1_BR			3
-#define SPI_CR1_SPE			6
-#define SPI_CR1_LSB			7
-#define SPI_CR1_SSI			8
-#define SPI_CR1_SSM			9
-#define SPI_CR1_RXONLY		10
-#define SPI_CR1_DFF			11
-#define SPI_CR1_CRCNEXT		12
-#define SPI_CR1_CRCEN		13
-#define SPI_CR1_BIDIOE		14
-#define SPI_CR1_BIDIMODE	15
-
-
-//bit definition mcro for cr2
-
-#define SPI_CR2_RXMAEN		0
-#define SPI_CR2_TXMAEN		1
-#define SPI_CR2_SSOE		2
-#define SPI_CR2_FRF			4
-#define SPI_CR2_ERRIE		5
-#define SPI_CR2_RXEIE		6
-#define SPI_CR2_TXEIE		7
-
-//spi sr
-#define SPI_SR_RXNE			0
-#define SPI_SR_TXE			1
-#define SPI_SR_CHSIDE		2
-#define SPI_SR_UDR			3
-#define SPI_SR_CRCERR		4
-#define SPI_SR_MODEF		5
-#define SPI_SR_OVR			6
-#define SPI_SR_BSY			7
-#define SPI_SR_FRE			8
-
-
-#define SPI_RXE_FLAG		(1 << SPI_SR_RXNE)
-#define SPI_TXE_FLAG		(1 << SPI_SR_TXE)
-#define SPI_CHSIDE_FLAG		(1 << SPI_SR_CHSIDE)
-#define SPI_UDR_FLAG		(1 << SPI_SR_UDR)
-#define SPI_CRCERR_FLAG		(1 << SPI_SR_CRCERR)
-#define SPI_MODEF_FLAG		(1 << SPI_SR_MODEF)
-#define SPI_OVR_FLAG		(1 << SPI_SR_OVR)
-#define SPI_BSY_FLAG		(1 << SPI_SR_BSY)
-#define SPI_FRE_FLAG		(1 << SPI_SR_FRE)
-
-
-// API prototypes
+#include "stm32f407xx_spi_driver.h"
 
 
 // init deinit
-void 		SPI_PCLK_Control(SPI_RegDef_t *pSPIx, uint8_t En_Or_Di);
-void 		SPI_Init(SPI_Handle_t *pSPIHandle);
+void SPI_PCLK_Control(SPI_RegDef_t *pSPIx, uint8_t En_Or_Di)
+{
+	if(En_Or_Di == ENABLE)
+	{
+		if(pSPIx	==	SPI1)
+		{
+			SPI1_PCLK_EN();
+		}
+		else if(pSPIx	==	SPI2)
+		{
+			SPI2_PCLK_EN();
+		}
+		else if(pSPIx	==	SPI3)
+		{
+			SPI3_PCLK_EN();
+		}
+		else if(pSPIx	==	SPI4)
+		{
+			SPI4_PCLK_EN();
+		}
+		else if(pSPIx	==	SPI5)
+		{
+			SPI5_PCLK_EN();
+		}
+		else if(pSPIx	==	SPI6)
+		{
+			SPI6_PCLK_EN();
+		}
+	}
+	if(En_Or_Di == DISABLE)
+	{
+		if(pSPIx	==	SPI1)
+		{
+			SPI1_PCLK_DI();
+		}
+		else if(pSPIx	==	SPI2)
+		{
+			SPI2_PCLK_DI();
+		}
+		else if(pSPIx	==	SPI3)
+		{
+			SPI3_PCLK_DI();
+		}
+		else if(pSPIx	==	SPI4)
+		{
+			SPI4_PCLK_DI();
+		}
+		else if(pSPIx	==	SPI5)
+		{
+			SPI5_PCLK_DI();
+		}
+		else if(pSPIx	==	SPI6)
+		{
+			SPI6_PCLK_DI();
+		}
+	}
+}
+void SPI_Init(SPI_Handle_t *pSPIHandle)
+{
+	SPI_PCLK_Control(pSPIHandle->pSPIx, ENABLE);
+	uint32_t temp =0;
+	//device mode
+	temp |= pSPIHandle->SPI_Config.SPI_DviceMode << SPI_CR1_MSTR;
+	//bus config
+	if(pSPIHandle->SPI_Config.SPI_BusConfig == SPI_BUS_CONFIG_FD)
+	{
+		temp &= ~(1<<15);
+	}else if(pSPIHandle->SPI_Config.SPI_BusConfig == SPI_BUS_CONFIG_HD)
+	{
+		temp |= (1<<15);
+	}else if(pSPIHandle->SPI_Config.SPI_BusConfig == SPI_BUS_CONFIG_SIPLEX_RX_ONLY)
+	{
+		temp &= ~(1<<15);
+		temp |= (1<<10);
+	}
+
+	temp |= pSPIHandle->SPI_Config.SPI_SclkSpeed <<3;
+
+	temp |= pSPIHandle->SPI_Config.SPI_DFF <<SPI_CR1_DFF;
+
+	temp |= pSPIHandle->SPI_Config.SPI_CPOL <<SPI_CR1_CPOL;
+
+	temp |= pSPIHandle->SPI_Config.SPI_CPHA <<SPI_CR1_CPHA;
+
+	temp |= pSPIHandle->SPI_Config.SPI_SSM <<SPI_CR1_SSM;
+
+
+	pSPIHandle->pSPIx->CR1 |= temp;
+
+
+
+}
 void 		SPI_DeInit(SPI_RegDef_t *pSPIx);
+
+
+uint8_t  SPI_GetFlagStatus(SPI_RegDef_t *pSPIx, uint32_t flagName)
+{
+	if(pSPIx->SR & flagName)
+	{
+		return FLAG_SET;
+	}
+
+	return FLAG_RESET;
+}
 
 
 //data RX TX
 
-void SPI_SendData(SPI_RegDef_t *pSPIx, uint8_t *pTxBuffer, uint32_t Len);
+//this is blocking blocking// polling
+void SPI_SendData(SPI_RegDef_t *pSPIx, uint8_t *pTxBuffer, uint32_t Len)
+{
+	while(Len >0)
+	{
+		while(SPI_GetFlagStatus(pSPIx, SPI_TXE_FLAG) == FLAG_RESET);
+
+		if((pSPIx->CR1 & (1<<SPI_CR1_DFF)))
+		{
+			pSPIx->DR = *((uint16_t*)(pTxBuffer));
+			Len--;
+			Len--;
+			(uint16_t*)pTxBuffer++;
+		}else
+		{
+			pSPIx->DR = *pTxBuffer;
+			Len--;
+			pTxBuffer++;
+		}
+	}
+}
+
+void SPI_Peri_Control(SPI_RegDef_t *pSPIx, uint8_t En_Or_Di)
+{
+	if(En_Or_Di == ENABLE)
+	{
+		pSPIx->CR1 |= (1 << SPI_CR1_SPE);
+	}else
+	{
+		pSPIx->CR1 &= ~(1 << SPI_CR1_SPE);
+	}
+}
+
+void SPI_SSI_Control(SPI_RegDef_t *pSPIx, uint8_t En_Or_Di)
+{
+	if(En_Or_Di == ENABLE)
+	{
+		pSPIx->CR1 |= (1 << SPI_CR1_SSI);
+	}else
+	{
+		pSPIx->CR1 &= ~(1 << SPI_CR1_SSI);
+	}
+}
+
 void SPI_ReceiveData(SPI_RegDef_t *pSPIx,uint8_t *pRxBuffer, uint32_t Len);
 
 //IRQ config
@@ -98,13 +168,3 @@ void SPI_ReceiveData(SPI_RegDef_t *pSPIx,uint8_t *pRxBuffer, uint32_t Len);
 void 		SPI_IRQInterruptConfig(uint8_t IRQNumber, uint8_t En_Or_Di);
 void 		SPI_IRQPriorityConfig(uint8_t IRQNumber, uint16_t IRQPriority);
 void 		SPI_IRQHandling(SPI_Handle_t *pSPIHandle);
-
-
-//other perri control
-
-void SPI_Peri_Control(SPI_RegDef_t *pSPIx, uint8_t En_Or_Di);
-
-void SPI_SSI_Control(SPI_RegDef_t *pSPIx, uint8_t En_Or_Di);
-
-
-#endif /* INC_STM32F407XX_SPI_DRIVER_H_ */
