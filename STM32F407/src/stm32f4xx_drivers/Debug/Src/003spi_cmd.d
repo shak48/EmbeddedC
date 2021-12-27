383a8da4ecb5c60bd699a6f48103c653d436b823
shak48 <rumie.kabir@gmail.com>
2021-12-27 17:31:00 -0500

    rearranged files

---
+++ STM32F407/src/stm32f4xx_drivers/Debug/Src/003spi_cmd.d
@@ -0,0 +1,19 @@
+Src/003spi_cmd.o: ../Src/003spi_cmd.c \
+ /home/shak48/Documents/local-repo/EmbeddedC/STM32F407/src/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_gpio_driver.h \
+ /home/shak48/Documents/local-repo/EmbeddedC/STM32F407/src/stm32f4xx_drivers/Drivers/Inc/stm32f407xx.h \
+ /home/shak48/Documents/local-repo/EmbeddedC/STM32F407/src/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_gpio_driver.h \
+ /home/shak48/Documents/local-repo/EmbeddedC/STM32F407/src/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_spi_driver.h \
+ /home/shak48/Documents/local-repo/EmbeddedC/STM32F407/src/stm32f4xx_drivers/Drivers/Inc/stm32f407xx.h \
+ /home/shak48/Documents/local-repo/EmbeddedC/STM32F407/src/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_spi_driver.h
+
+/home/shak48/Documents/local-repo/EmbeddedC/STM32F407/src/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_gpio_driver.h:
+
+/home/shak48/Documents/local-repo/EmbeddedC/STM32F407/src/stm32f4xx_drivers/Drivers/Inc/stm32f407xx.h:
+
+/home/shak48/Documents/local-repo/EmbeddedC/STM32F407/src/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_gpio_driver.h:
+
+/home/shak48/Documents/local-repo/EmbeddedC/STM32F407/src/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_spi_driver.h:
+
+/home/shak48/Documents/local-repo/EmbeddedC/STM32F407/src/stm32f4xx_drivers/Drivers/Inc/stm32f407xx.h:
+
+/home/shak48/Documents/local-repo/EmbeddedC/STM32F407/src/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_spi_driver.h:
--- STM32F407/src/stm32f4xx_drivers/Debug/Src/subdir.mk
+++ STM32F407/src/stm32f4xx_drivers/Debug/Src/subdir.mk
@@ -5,31 +5,23 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../Src/button_interrupt_PA0.c \
-../Src/syscalls.c \
-../Src/sysmem.c 
+../Src/003spi_cmd.c 
 
 OBJS += \
-./Src/button_interrupt_PA0.o \
-./Src/syscalls.o \
-./Src/sysmem.o 
+./Src/003spi_cmd.o 
 
 C_DEPS += \
-./Src/button_interrupt_PA0.d \
-./Src/syscalls.d \
-./Src/sysmem.d 
+./Src/003spi_cmd.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
-Src/button_interrupt_PA0.o: ../Src/button_interrupt_PA0.c Src/subdir.mk
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DSTM32 -DSTM32F407G_DISC1 -DSTM32F4 -DSTM32F407VGTx -c -I../Inc -I"/home/shak48/Documents/shak48-repos/EmbeddedC/STM32F407/src/stm32f4xx_drivers/Drivers/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
-Src/%.o: ../Src/%.c Src/subdir.mk
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DSTM32 -DSTM32F407G_DISC1 -DSTM32F4 -DSTM32F407VGTx -c -I../Inc -I"/home/shak48/Documents/shak48-repos/EmbeddedC/STM32F407/src/stm32f4xx_drivers/Drivers/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Src/003spi_cmd.o: ../Src/003spi_cmd.c Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DSTM32 -DSTM32F407G_DISC1 -DSTM32F4 -DSTM32F407VGTx -c -I../Inc -I"/home/shak48/Documents/local-repo/EmbeddedC/STM32F407/src/stm32f4xx_drivers/Drivers/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
 
 clean: clean-Src
 
 clean-Src:
-	-$(RM) ./Src/button_interrupt_PA0.d ./Src/button_interrupt_PA0.o ./Src/syscalls.d ./Src/syscalls.o ./Src/sysmem.d ./Src/sysmem.o
+	-$(RM) ./Src/003spi_cmd.d ./Src/003spi_cmd.o
 
 .PHONY: clean-Src
 
