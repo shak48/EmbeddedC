4418932a49f5bf26e9fa0d7550c5a5223218247a
shak48 <rumie.kabir@gmail.com>
2022-01-03 18:58:10 -0500

    Added project file

--- STM32F407/Src/FreeRtos_Projects/Debug/ThirdParty/FreeRtos/list.d
+++ STM32F407/Src/FreeRtos_Projects/Debug/ThirdParty/FreeRtos/list.d
@@ -1,25 +1,25 @@
 ThirdParty/FreeRtos/list.o: ../ThirdParty/FreeRtos/list.c \
- /home/shak48/Documents/shak48-repos/EmbeddedC/STM32F407/FreeRtos_Projects/ThirdParty/FreeRtos/include/FreeRTOS.h \
- /home/shak48/Documents/shak48-repos/EmbeddedC/STM32F407/FreeRtos_Projects/ThirdParty/FreeRtos/FreeRTOSConfig.h \
- /home/shak48/Documents/shak48-repos/EmbeddedC/STM32F407/FreeRtos_Projects/ThirdParty/FreeRtos/include/projdefs.h \
- /home/shak48/Documents/shak48-repos/EmbeddedC/STM32F407/FreeRtos_Projects/ThirdParty/FreeRtos/include/portable.h \
- /home/shak48/Documents/shak48-repos/EmbeddedC/STM32F407/FreeRtos_Projects/ThirdParty/FreeRtos/include/deprecated_definitions.h \
- /home/shak48/Documents/shak48-repos/EmbeddedC/STM32F407/FreeRtos_Projects/ThirdParty/FreeRtos/portable/GCC/ARM_CM4F/portmacro.h \
- /home/shak48/Documents/shak48-repos/EmbeddedC/STM32F407/FreeRtos_Projects/ThirdParty/FreeRtos/include/mpu_wrappers.h \
- /home/shak48/Documents/shak48-repos/EmbeddedC/STM32F407/FreeRtos_Projects/ThirdParty/FreeRtos/include/list.h
+ /home/shak48/Documents/local-repo/EmbeddedC/STM32F407/Src/FreeRtos_Projects/ThirdParty/FreeRtos/include/FreeRTOS.h \
+ /home/shak48/Documents/local-repo/EmbeddedC/STM32F407/Src/FreeRtos_Projects/ThirdParty/FreeRtos/FreeRTOSConfig.h \
+ /home/shak48/Documents/local-repo/EmbeddedC/STM32F407/Src/FreeRtos_Projects/ThirdParty/FreeRtos/include/projdefs.h \
+ /home/shak48/Documents/local-repo/EmbeddedC/STM32F407/Src/FreeRtos_Projects/ThirdParty/FreeRtos/include/portable.h \
+ /home/shak48/Documents/local-repo/EmbeddedC/STM32F407/Src/FreeRtos_Projects/ThirdParty/FreeRtos/include/deprecated_definitions.h \
+ /home/shak48/Documents/local-repo/EmbeddedC/STM32F407/Src/FreeRtos_Projects/ThirdParty/FreeRtos/portable/GCC/ARM_CM4F/portmacro.h \
+ /home/shak48/Documents/local-repo/EmbeddedC/STM32F407/Src/FreeRtos_Projects/ThirdParty/FreeRtos/include/mpu_wrappers.h \
+ /home/shak48/Documents/local-repo/EmbeddedC/STM32F407/Src/FreeRtos_Projects/ThirdParty/FreeRtos/include/list.h
 
-/home/shak48/Documents/shak48-repos/EmbeddedC/STM32F407/FreeRtos_Projects/ThirdParty/FreeRtos/include/FreeRTOS.h:
+/home/shak48/Documents/local-repo/EmbeddedC/STM32F407/Src/FreeRtos_Projects/ThirdParty/FreeRtos/include/FreeRTOS.h:
 
-/home/shak48/Documents/shak48-repos/EmbeddedC/STM32F407/FreeRtos_Projects/ThirdParty/FreeRtos/FreeRTOSConfig.h:
+/home/shak48/Documents/local-repo/EmbeddedC/STM32F407/Src/FreeRtos_Projects/ThirdParty/FreeRtos/FreeRTOSConfig.h:
 
-/home/shak48/Documents/shak48-repos/EmbeddedC/STM32F407/FreeRtos_Projects/ThirdParty/FreeRtos/include/projdefs.h:
+/home/shak48/Documents/local-repo/EmbeddedC/STM32F407/Src/FreeRtos_Projects/ThirdParty/FreeRtos/include/projdefs.h:
 
-/home/shak48/Documents/shak48-repos/EmbeddedC/STM32F407/FreeRtos_Projects/ThirdParty/FreeRtos/include/portable.h:
+/home/shak48/Documents/local-repo/EmbeddedC/STM32F407/Src/FreeRtos_Projects/ThirdParty/FreeRtos/include/portable.h:
 
-/home/shak48/Documents/shak48-repos/EmbeddedC/STM32F407/FreeRtos_Projects/ThirdParty/FreeRtos/include/deprecated_definitions.h:
+/home/shak48/Documents/local-repo/EmbeddedC/STM32F407/Src/FreeRtos_Projects/ThirdParty/FreeRtos/include/deprecated_definitions.h:
 
-/home/shak48/Documents/shak48-repos/EmbeddedC/STM32F407/FreeRtos_Projects/ThirdParty/FreeRtos/portable/GCC/ARM_CM4F/portmacro.h:
+/home/shak48/Documents/local-repo/EmbeddedC/STM32F407/Src/FreeRtos_Projects/ThirdParty/FreeRtos/portable/GCC/ARM_CM4F/portmacro.h:
 
-/home/shak48/Documents/shak48-repos/EmbeddedC/STM32F407/FreeRtos_Projects/ThirdParty/FreeRtos/include/mpu_wrappers.h:
+/home/shak48/Documents/local-repo/EmbeddedC/STM32F407/Src/FreeRtos_Projects/ThirdParty/FreeRtos/include/mpu_wrappers.h:
 
-/home/shak48/Documents/shak48-repos/EmbeddedC/STM32F407/FreeRtos_Projects/ThirdParty/FreeRtos/include/list.h:
+/home/shak48/Documents/local-repo/EmbeddedC/STM32F407/Src/FreeRtos_Projects/ThirdParty/FreeRtos/include/list.h:
